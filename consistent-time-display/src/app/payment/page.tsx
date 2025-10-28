6ca82006babb79870e8296869181034399d246e9
Rafsan2507 <khanrafsan25@gmail.com>
2025-10-28 17:17:54 +0600

    removed unnecessary file

--- consistent-time-display/src/app/payment/page.tsx
+++ consistent-time-display/src/app/payment/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-// import Dsas from '@/components/DeliveryTime/Dsas'
 import Payment from "@/components/PaymentDeadline/Payment";
 type Props = {};
 
 const page = (props: Props) => {
   return (
-    // <Dsas/>
     <Payment />
   );
 };
